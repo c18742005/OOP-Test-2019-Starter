a27957d157471de0247b1059cffdc9fe504438ce
C18742005 <c18742005@mytudublin.ie>
2020-04-21 22:12:50 +0100

    added notes to code

--- src/ie/tudublin/Resistor.java
+++ src/ie/tudublin/Resistor.java
@@ -17,18 +17,20 @@ public class Resistor {
         ui.pushMatrix();
         ui.translate(x, y);
         ui.stroke(0);
+        
+        // draw the resistor shape
         ui.line(-100, 0, -50, 0);
         ui.line(-50, 0, -50, -50);
         ui.line(-50, -50, 50, -50);
-        
         ui.line(50, -50, 50, 0);
         ui.line(50, 0, 100, 0);
         ui.line(50, 0, 50, 50);
         ui.line(50, 50, -50, 50);
         ui.line(-50, 50, -50, 0);
 
+        ui.noStroke(); // no outline of shapes
+
         // Draw the color bars
-        ui.noStroke();
         ui.fill(hc.r, hc.g, hc.b);
         ui.rect(-40, -49, 10, 99);
         
@@ -37,10 +39,13 @@ public class Resistor {
         
         ui.fill(oc.r, oc.g, oc.b);
         ui.rect(0, -49, 10, 99);
+
+        // draw the value of the resistor to screen
         ui.fill(0);
         ui.textAlign(PApplet.CENTER, PApplet.CENTER);
         ui.textSize(30);
         ui.text(value, 200, 0);
+
         ui.popMatrix();
     }
 
